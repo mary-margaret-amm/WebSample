6af26489c9d61235e952389da97aeee10f977dd2
mary-margaret-amm <maggy.amm@gmail.com>
2025-07-09 14:20:11 +0530

    Additional Scenarios

--- src/test/java/com/StepDefinition/TestCasePage.java
+++ src/test/java/com/StepDefinition/TestCasePage.java
@@ -4,9 +4,12 @@ import java.io.File;
 import java.io.IOException;
 import java.time.Duration;
 import java.util.List;
+import java.util.Map;
 
 import org.junit.Assert;
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
@@ -16,6 +19,7 @@ import org.openqa.selenium.io.FileHandler;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import io.cucumber.datatable.DataTable;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -127,11 +131,124 @@ public class TestCasePage {
 
     }
 
-    // WebElement cartSuccess = driver.findElement(By.xpath("//p[text()='Your
-    // product has been added to cart.']"));
-    // String expected = "Your product has been added to cart.";
-    // String actual = cartSuccess.getText();
-    // Assert.assertEquals(expected,actual);
-    // ("//h4[text()='Added!']")
+    @When("Click on Contact us button")
+    public void click_on_contact_us_button() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a/child::i[contains(@class,'fa fa-envelope')]")))
+                .click();
+
+    }
+
+    @When("fill the details in the form")
+    public void fill_the_details_in_the_form(DataTable dt) throws InterruptedException {
+        List<String> li = dt.asList();
+        driver.findElement(By.name("name")).sendKeys(li.get(0));
+        Thread.sleep(2000);
+        driver.findElement(By.name("email")).sendKeys(li.get(1));
+        Thread.sleep(2000);
+        driver.findElement(By.name("subject")).sendKeys(li.get(2));
+        Thread.sleep(2000);
+        driver.findElement(By.id("message")).sendKeys(li.get(3));
+        Thread.sleep(2000);
+
+    }
+
+    @When("click the submit button")
+    public void click_the_submit_button() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@name='submit']"))).click();
+
+    }
+
+    @When("click on ok button")
+    public void click_on_ok_button() throws InterruptedException {
+        Thread.sleep(3000);
+        Alert simpleAlert = driver.switchTo().alert();
+        simpleAlert.accept();
+        System.out.println("Alert handled");
+
+    }
+
+    @Then("Verify success message {string} is visible")
+    public void verify_success_message_is_visible(String data1) throws InterruptedException {
+        System.out.println(data1);
+    }
+
+    @Then("Click Home button and verify that landed to home page successfully {string}")
+    public void click_home_button_and_verify_that_landed_to_home_page_successfully(String data2)
+            throws InterruptedException {
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//i[contains(@class,'fa fa-angle-double-left')]")).click();
+        System.out.println(data2);
+    }
+
+    @When("Click Signup \\/ Login button")
+    public void click_signup_login_button() {
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//i[contains(@class,'fa fa-lock')]"))).click();
+
+    }
+
+    @When("Fill all details in Signup and the create account")
+    public void fill_all_details_in_signup_and_the_create_account(DataTable dt) throws InterruptedException {
+        Map<String, String> mp = dt.asMap();
+        driver.findElement(By.name("name")).sendKeys(mp.get("Name"));
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//input[@type='email'][@name='email'][@data-qa='signup-email']"))
+                .sendKeys(mp.get("email"));
+        driver.findElement(By.xpath("//button[@type='submit'][text()='Signup']")).click();
+
+    }
+
+    @When("Enter account information1")
+    public void enter_account_information1(DataTable dataTable) throws InterruptedException {
+
+        driver.findElement(By.xpath("//input[@type='radio'][@id='id_gender2']")).click();
+        Thread.sleep(2000);
+        // driver.findElement(By.id("name")).sendKeys("Mary Margaret");
+        // Thread.sleep(2000);
+        // driver.findElement(By.id("email")).sendKeys("marymargaret.ap@gmail.com");
+        // Thread.sleep(2000);
+        driver.findElement(By.name("password")).sendKeys("Mary@123");
+        Thread.sleep(2000);
+        driver.findElement(By.id("days")).sendKeys("20");
+        Thread.sleep(2000);
+        driver.findElement(By.id("months")).sendKeys("August");
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//select[@data-qa='years'][@id='years']")).sendKeys("2001");
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//input[@type='checkbox'][@value='1'][@id='newsletter']")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//input[@type='checkbox'][@value='1'][@id='optin']")).click();
+        Thread.sleep(2000);
+        List<WebElement> li1 = driver.findElements(By.xpath("//p[@class='required form-group']"));
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("arguments[0].scrollIntoView(false)",
+                driver.findElement(By.xpath("//input[contains(@id,'first_name')][@type='text']")));
+        Thread.sleep(2000);
+        System.out.println("a");
+        for (WebElement x : li1) {
+            List<String> list = dataTable.asList();
+            System.out.println(x.getText());
+            x.sendKeys("Mary");
+            System.out.println("bb");
+
+            for (int i = 0; i < list.size(); i++) {
+                js.executeScript("arguments[0].setAttribute('value','list.get(i)')", x);
+                Thread.sleep(2000);
+
+                // x.sendKeys("list.get(i)");
+
+            }
+
+            // for (String y : list) {
+            // x.sendKeys(y);
+            // Thread.sleep(2000);
+            // }
+        }
+        System.out.println("success");
+
+    }
 
 }
--- target/test-classes/Feature/TestCasePage.feature
+++ target/test-classes/Feature/TestCasePage.feature
@@ -1,5 +1,6 @@
 
-Feature: Mary_Web_Validating Test Case Page and Product Detail page
+Feature: Mary_Web_Test Case Page, Product Detail page, Add to cart, contact us form, SignUp details
+
 
     #   Background: launching url
     #      Given launching the url "https://www.automationexercise.com/"
@@ -30,8 +31,42 @@ Feature: Mary_Web_Validating Test Case Page and Product Detail page
         And click on Add to cart button
         Then verify if the product is added to cart successfully
 
+    @reg_4
+    Scenario: Contact us form
+        Given launching the url "https://www.automationexercise.com/"
+        When verify that the home page is visible successfully
+        And Click on Contact us button
+        And fill the details in the form
+            | Mary Margaret                       |
+            | marymargaret.albert@expleogroup.com |
+            | sample                              |
+            | message                             |
 
+        And click the submit button
+        And click on ok button
+        Then Verify success message "Success! Your details have been submitted successfully." is visible
+        And Click Home button and verify that landed to home page successfully "home"
 
+    @reg_5
+    Scenario: Signup page
+        Given launching the url "https://www.automationexercise.com/"
+        When verify that the home page is visible successfully
+        And Click Signup / Login button
+        And Fill all details in Signup and the create account
+            | Name  | Margaret                  |
+            | email | marymargaret.ap@gmail.com |
+        And Enter account information1
+            | Mary       |
+            | Margaret   |
+            | XXX        |
+            | YYY        |
+            | YYY        |
+            | India      |
+            | TamilNadu  |
+            | Chennai    |
+            | 600041     |
+            | 9876543210 |
+      
 
 
 
