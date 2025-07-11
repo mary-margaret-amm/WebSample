ef919f8f66aad7c10517aeaae0c05ecaef61d5ff
dhilip19031991 <dhilip.jaishankar17@gmail.com>
2025-07-11 15:34:43 +0530

    Product feature checking first product details

---
+++ src/test/java/com/StepDefinition/ProdctDetailsSteps.java
@@ -0,0 +1,33 @@
+package com.StepDefinition;
+
+import com.example.BaseClass;
+import com.example.pages.ProductDetails;
+
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class ProdctDetailsSteps extends BaseClass {
+ProductDetails productdetails;
+
+@Then("verify product option is visible")
+public void verify_product_option_is_visible() {
+    productdetails=new ProductDetails(driver);
+    productdetails.ProductOptionVisible();
+}
+@When("click on product option")
+public void click_on_product_option() {
+    productdetails.ProductOptionClick();
+}
+@Then("verify user is navigated to all products page")
+public void verify_user_is_navigated_to_all_products_page() {
+    productdetails.AllProductVisible();
+}
+@When("Click on View Product of first product")
+public void click_on_view_product_of_first_product() {
+    productdetails.FirstViewProdcutClick();
+}
+@Then("verify product name, price and availability")
+public void verify_product_name_price_and_availability() {
+   productdetails.FirstProductDetails();
+}
+}
