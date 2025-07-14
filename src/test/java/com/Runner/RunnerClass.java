da69ffb32852da7142d1898c25c6889d813caa6c
mary-margaret-amm <maggy.amm@gmail.com>
2025-07-14 14:42:53 +0530

    recent

--- src/test/java/com/Runner/RunnerClass.java
+++ src/test/java/com/Runner/RunnerClass.java
@@ -6,15 +6,14 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources/Feature", glue = "com.StepDefinition", dryRun = false,
-        // tags = "@sanity_2",
-        // monochrome = true,
-        // publish = true
+@CucumberOptions(features = "src/test/resources/Feature", glue = "com.StepDefinition", dryRun = false, tags = "@sanity",
+                // monochrome = true,
+                // publish = true
 
-        plugin = {
-                "json:target/cucumber-report/cucumber.json",
-                "html:target/cucumber-report/cucumber.html"
-        })
+                plugin = {
+                                "json:target/cucumber-report/cucumber.json",
+                                "html:target/cucumber-report/cucumber.html"
+                })
 public class RunnerClass {
 
 }
